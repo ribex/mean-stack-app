ab5176999a5801d759e69a9415793169eaecdc26
ribex <ribexy@gmail.com>
2017-05-20 18:23:33 -0400

    Setting up Express router

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var routes = require('./routes');
+
 app.set('port', 3000);
 
 app.use(function(req, res, next) {
@@ -10,6 +12,8 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
 
 app.get('/json', function (req, res) {
 	console.log("GET the json");
