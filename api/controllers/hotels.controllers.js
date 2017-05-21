8c177ecec19a0cdcb960a3452a166dd0dd16838f
ribex <ribexy@gmail.com>
2017-05-20 22:15:59 -0400

    Defined dynamic routes with URL parameters

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,6 +1,17 @@
+var hotelData = require('../data/hotel-data.json');
+
 module.exports.hotelsGetAll = function(req, res) {
-    console.log("GET the json");
+    console.log("GET the hotels");
     res
       .status(200)
-      .json( {"jsonData" : true} )
+      .json( hotelData );
+};
+
+module.exports.hotelsGetOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+    console.log("GET hotelId", hotelId);
+    res
+      .status(200)
+      .json( thisHotel );
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 var ctrlHotels = require ('../controllers/hotels.controllers.js');
 
 router
-  .route('/json')
+  .route('/hotels')
   .get(ctrlHotels.hotelsGetAll);
+
+router
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne);
 
 module.exports = router;
