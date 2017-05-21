611e3073d5644d25169c2cb4441a7c442bded8c3
ribex <ribexy@gmail.com>
2017-05-20 22:37:15 -0400

    Working with querystrings and posted form data

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -2,9 +2,24 @@ var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
     console.log("GET the hotels");
+    console.log(req.query);
+
+    var offset = 0;
+    var count = 5;
+
+    if (req.query && req.query.offset) {
+        offset = parseInt(req.query.offset, 10);
+    }
+
+    if (req.query && req.query.count) {
+        count = parseInt(req.query.count, 10);
+    }
+
+    var returnData = hotelData.slice(offset, offset+count);
+
     res
       .status(200)
-      .json( hotelData );
+      .json( returnData );
 };
 
 module.exports.hotelsGetOne = function(req, res) {
@@ -14,4 +29,12 @@ module.exports.hotelsGetOne = function(req, res) {
     res
       .status(200)
       .json( thisHotel );
+};
+
+module.exports.hotelsAddOne = function(req, res) {
+    console.log("POST new hotel");
+    console.log(req.body);
+    res
+        .status(200)
+        .json(req.body);
 };
