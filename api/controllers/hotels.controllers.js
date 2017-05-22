fa47183a11301832281c6c8aef18f5f529a0445e
ribex <ribexy@gmail.com>
2017-05-22 19:53:17 -0400

    Querying single document from database

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,4 +1,5 @@
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
@@ -31,12 +32,22 @@ module.exports.hotelsGetAll = function(req, res) {
  };
 
 module.exports.hotelsGetOne = function(req, res) {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
-    res
-      .status(200)
-      .json( thisHotel );
+    
+    collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        }, function(err, doc) {
+            res
+                .status(200)
+                .json( doc );
+        })
+    
+
 };
 
 module.exports.hotelsAddOne = function(req, res) {
