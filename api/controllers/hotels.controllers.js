c839c51030b67900768dee6c687a5439312f7c8f
ribex <ribexy@gmail.com>
2017-05-24 17:11:41 -0400

    Error trapping

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -32,6 +32,7 @@ module.exports.hotelsGetAll = function(req, res) {
 
     var offset = 0;
     var count = 5;
+    var maxCount = 10;
 
     if (req.query && req.query.lng && req.query.lat) {
         runGeoQuery(req, res);
@@ -46,14 +47,39 @@ module.exports.hotelsGetAll = function(req, res) {
         count = parseInt(req.query.count, 10);
     }
 
+    if (isNaN(offset) || isNaN(count)) {
+        res
+            .status(400)
+            .json({
+                "message" : "If supplied in querystring, count and offset should be numbers."
+            });
+        return;
+    }
+
+    if (count > maxCount) {
+        res
+            .status(400)
+            .json({
+                "message" : "Count limit of " + maxCount + " exceeded"
+            });
+        return;
+    }
+
     Hotel
         .find()
         .skip(offset)
         .limit(count)  
         .exec(function(err, hotels) {
+            if (err) {
+                console.log("Error finding hotels");
+                res
+                    .status(500)
+                    .json(err);
+            } else {
             console.log("Found hotels", hotels.length);
             res
                 .json(hotels);
+            }
         });
  };
 
@@ -64,9 +90,23 @@ module.exports.hotelsGetOne = function(req, res) {
     Hotel
         .findById(hotelId)
         .exec(function(err, doc) {
+            var response = {
+                status : 200,
+                message : doc
+            };
+            if (err) {
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } else if (!doc) {
+                respose.status = 404;
+                response.message = {
+                    "message" : "Hotel ID not found"
+                };            
+            }
             res
-                .status(200)
-                .json( doc );
+                .status(response.status)
+                .json(response.message);
         });
 };
 
