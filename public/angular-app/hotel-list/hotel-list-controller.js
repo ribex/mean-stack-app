984a3324042d9a50b768227aa621fc9743d34d33
ribex <ribexy@gmail.com>
2017-06-05 16:58:52 -0400

    Pulling in API data with GET request in controller and display hotel names on page

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -1,6 +1,10 @@
 angular.module('meanhotel').controller('HotelsController', HotelsController);
 
-function HotelsController() {
+function HotelsController($http) {
     var vm = this;
     vm.title = 'MEAN Hotel App is awesome';
+    $http.get('/api/hotels?count=10').then(function(response) {
+        // console.log(response); // browser console
+        vm.hotels = response.data;
+    });
 }
