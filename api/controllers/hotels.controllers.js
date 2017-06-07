5b14974a6c5ea188a62b21b68cf609fa263aacef
ribex <ribexy@gmail.com>
2017-06-06 21:15:19 -0400

    Adding user authentication to view hotels

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -44,6 +44,9 @@ var runGeoQuery = function(req, res) {
 };
 
 module.exports.hotelsGetAll = function(req, res) {
+    console.log('Requested by: ' + req.user);
+    console.log('GET the hotels');
+    console.log(req.query);
 
     var offset = 0;
     var count = 5;
